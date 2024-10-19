2c451e2e0d08402e0688f7e482878c6bcd63e597
6hartmann <business@okaycha.mp>
2024-10-19 17:09:41 +0200

    patch: add missing comment and rewrite

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { Client, Events, ActivityType, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
 
-// define french users of bob tuah cord
+// define french users you want to track
 const french = ['224191281205739520'];
 
 // log into the client blah blah
@@ -23,6 +23,11 @@ client.once(Events.ClientReady, readyClient => {
     });
 });
 
+/*
+    check for messages from the french and respond to them
+    of course, that emote in this repo is from bobnetwork's discord server
+    adjust the message however you want it :)
+*/
 client.on('messageCreate', async message => {
     if(french.includes(message.author.id) && message.author.id != client.user.id){
         await message.reply('france sucks. <:CatFuckYou:1280928034601308231>');
